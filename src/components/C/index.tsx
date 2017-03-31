541335443915a1364d797b3f6bce2d5de5dfb262
Philipp A <flying-sheep@web.de>
2017-03-31 09:54:43 +0200

    fixed resolution problems

--- src/components/C/index.tsx
+++ src/components/C/index.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react'
 
-export default class C extends React.Component {
+export default class C extends React.Component<{},{}> {
 	render() {
 		return <span className='test'/>
 	}
